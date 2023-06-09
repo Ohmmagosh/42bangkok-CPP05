9aed7bddcd043a2e40c5a37a3a18f16a33cdb4eb
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-06-09 20:04:02 +0700

    ADD ex00 custom try exception

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 21:01:37 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/03/13 21:10:11 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/06/09 19:57:21 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,31 @@
 # define BUREAUCRAT_HPP
 
 # include <iostream>
+# include <exception>
 
 class Bureaucrat {
 	private:
+		const std::string	_name;
+		int					_grade;
 	public:
 		Bureaucrat( void );
 		Bureaucrat( const Bureaucrat &cp );
+		Bureaucrat( std::string name, int grade );
 		Bureaucrat& operator=( const Bureaucrat &cp );
 		~Bureaucrat( );
-
+		std::string getName( void ) const;
+		int getGrade( void ) const;
+	class GradeTooHighException:public std::exception {
+		const char * what () const throw () {
+			return "Grade too high!!";
+		};
+	};
+	class GradeTooLowException:public std::exception {
+		const char * what () const throw () {
+			return "Grade too low!!";
+		};
+	};
 };
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat &cp);
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,17 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 21:01:32 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/03/13 21:10:47 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/06/09 20:02:26 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int	main(void){
-
-	std::cout << "hello" << std::endl;
+int	main(void) {
+	{
+		try {
+			Bureaucrat a("twenty", 20);
+		} catch (const std::exception &e ) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try {
+			Bureaucrat a("zero", 0);
+		} catch (const std::exception &e ) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try {
+			Bureaucrat a("twohundred", 200);
+		} catch (const std::exception &e ) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try
+		{
+			Bureaucrat a("Fourty", 40);
+			std::cout << a ;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 	return (0);
 }
